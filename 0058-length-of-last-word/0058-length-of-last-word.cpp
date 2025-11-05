82821446228f87b4bc6701143cc3d63ce78b621f
NScoded <nileshsahu2005@gmail.com>
2025-11-05 12:50:05 +0530

    Time: 0 ms (100%), Space: 9 MB (10.46%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int size=0;
+        bool ready=0;
+        for(int i=s.size()-1;i>-1;i--){
+            if((s[i]==' '|| s[i]=='.' )&& (ready==0) ){
+                continue;
+            }
+            ready=1;
+            if(s[i]!=' '){
+                size++;
+                continue;
+            }
+            break;
+        }
+        return size;
+        
+    }
+};
