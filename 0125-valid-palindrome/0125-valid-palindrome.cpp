e5ae3c30b864f2171ab448abc7094a5aa4d882ac
NScoded <nileshsahu2005@gmail.com>
2025-11-27 22:27:08 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-    int i = 0, j = s.size() - 1;
-
-    while (i < j) {
-
-        // move left pointer to next alphanumeric
-        while (i < j && !isalnum(s[i])) i++;
-
-        // move right pointer to next alphanumeric
-        while (i < j && !isalnum(s[j])) j--;
-
-        // compare lowercased characters
-        if (tolower(s[i]) != tolower(s[j]))
-            return false;
-
-        i++;
-        j--;
-    }
-    return true;
+        int i=0,j=s.size()-1;
+        while(i<j){
+            while(j>i && !isalnum(s[j])) j--;
+            while(i<j && !isalnum(s[i]))i++;
+            if(tolower(s[i])!=tolower(s[j])) return false;
+            i++;j--;
+        }
+        return true;
 }
 
 };
