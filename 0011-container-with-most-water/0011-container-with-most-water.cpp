010ad795dd2aab9c0f79b08c044921f03a8112f9
NScoded <nileshsahu2005@gmail.com>
2025-12-01 23:40:14 +0530

    Time: 1 ms (45.38%), Space: 63 MB (47.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int l=0,r=height.size()-1,best=0;
-
-        while(r>l){
-            best=max(best,(min(height[l],height[r]))*(r-l));
-            (height[l]>height[r])? r--:l++;
+        int l=0,r=height.size()-1;
+        int best=0;
+        while(l<r){
+            best=max(best,min(height[r],height[l])*(r-l));
+            (height[l]<height[r])?l++:r--;
         }
         return best;
     }
