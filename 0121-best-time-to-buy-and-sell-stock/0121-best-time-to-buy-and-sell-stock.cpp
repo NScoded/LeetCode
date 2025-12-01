8e8c319308835594b060f7de0d92a3117c115042
NScoded <nileshsahu2005@gmail.com>
2025-12-01 23:20:48 +0530

    Time: 3 ms (28.6%), Space: 97.4 MB (60.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int buy=INT_MAX;
         int sell=0;
         for(int i=0;i<prices.size();i++){
-            if(prices[i]<buy) buy=prices[i];
+            buy=min(buy,prices[i]);
             sell=max(sell,prices[i]-buy);
         }
         return sell;
