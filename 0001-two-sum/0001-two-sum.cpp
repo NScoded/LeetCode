442c1af13081b2d277f81c0dbf471df141edf382
NScoded <nileshsahu2005@gmail.com>
2025-12-04 11:49:35 +0530

    Time: 0 ms (100%), Space: 14.9 MB (36.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+        unordered_map<int, int> mp;  // value → index
+        
+        for(int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            
+            if(mp.count(need)) {         // if complement exists
+                return {mp[need], i};
             }
+            
+            mp[nums[i]] = i;             // store current value
         }
+        
         return {};
     }
 };
