1fc4489c6f9e12d5556b01d8a8d58a580d54d02f
NScoded <nileshsahu2005@gmail.com>
2025-12-27 16:16:10 +0530

    Time: 66 ms (77.85%), Space: 98.2 MB (70.22%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,61 +1,56 @@
-
-
-
-/// Solution 1
+// Solution 2
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>> meetings) {
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        // Sort meetings by start time
+        sort(meetings.begin(), meetings.end());
 
-    sort(meetings.begin(), meetings.end());
-
-    // free rooms (min room index)
-    priority_queue<int, vector<int>, greater<int>> freeRooms;
-
-    // busy rooms {endTime, roomIndex}
-    priority_queue<pair<long long,int>,
-        vector<pair<long long,int>>,
-        greater<pair<long long,int>>> busyRooms;
-
-    vector<int> count(n, 0);
-
-    // initially all rooms are free
-    for(int i = 0; i < n; i++)
-        freeRooms.push(i);
-
-    for(auto &m : meetings) {
-        long long start = m[0], end = m[1];
-        long long duration = end - start;
-
-        // free rooms that have finished
-        while(!busyRooms.empty() && busyRooms.top().first <= start) {
-            freeRooms.push(busyRooms.top().second);
-            busyRooms.pop();
+        // Min heap of available rooms
+        priority_queue<int, vector<int>, greater<int>> availableRooms;
+        for (int i = 0; i < n; i++) {
+            availableRooms.push(i);
         }
 
-        int room;
-        long long finishTime;
+        // Min heap of busy rooms: {endTime, roomIndex}
+        priority_queue<pair<long long, int>,
+            vector<pair<long long, int>>,
+            greater<pair<long long, int>>> busyRooms;
 
-        if(!freeRooms.empty()) {
-            room = freeRooms.top();
-            freeRooms.pop();
-            finishTime = end;
-        } else {
-            auto [t, r] = busyRooms.top();
-            busyRooms.pop();
-            room = r;
-            finishTime = t + duration;
+        vector<int> count(n, 0);
+
+        for (auto& meeting : meetings) {
+            long long start = meeting[0];
+            long long end = meeting[1];
+            long long duration = end - start;
+
+            // Free up rooms
+            while (!busyRooms.empty() && busyRooms.top().first <= start) {
+                availableRooms.push(busyRooms.top().second);
+                busyRooms.pop();
+            }
+
+            if (!availableRooms.empty()) {
+                int room = availableRooms.top();
+                availableRooms.pop();
+                busyRooms.push({end, room});
+                count[room]++;
+            } else {
+                // Delay meeting
+                auto [finishTime, room] = busyRooms.top();
+                busyRooms.pop();
+                busyRooms.push({finishTime + duration, room});
+                count[room]++;
+            }
         }
 
-        count[room]++;
-        busyRooms.push({finishTime, room});
-    }
+        // Find room with max meetings
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            if (count[i] > count[result]) {
+                result = i;
+            }
+        }
 
-    // find most booked room
-    int ans = 0;
-    for(int i = 1; i < n; i++) {
-        if(count[i] > count[ans])
-            ans = i;
+        return result;
     }
-    return ans;
-}
 };
