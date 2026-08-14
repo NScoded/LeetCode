a1ff3249b8363b169618c78545bb9c6859a6a773
Nilesh Sahu <nileshsahu2005@gmail.com>
2026-08-14 21:46:03 +0530

    Time: 546 ms (5.04%), Space: 90.6 MB (63.45%) - LeetHub

---
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int right=0;
+        int sum=0;
+        int down =0;
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+
+            if(sum+grid[i][0]<=k){
+                
+                sum+=grid[i][0];
+                if(i)ans++;
+        
+                down++;
+            }
+            else{
+                break;
+            }
+
+        }
+        
+    
+        
+        sum=0;
+        vector<int>buffer;
+        for(int i=0;i<grid[0].size();i++){
+            if(sum+grid[0][i]<=k){
+                sum+=grid[0][i];
+                ans++;
+                right++;
+                if(buffer.empty()){
+                    buffer.push_back(grid[0][i]);
+                }
+                else{
+                    buffer.push_back(buffer.back()+grid[0][i]);
+                }
+            }
+            else{
+                break;
+            }
+
+        }
+        if(down==0|| right==0)return ans;
+
+        for(int column=1;column<right;column++){
+            sum=buffer[column];
+            int j=column;
+            for(int row=1;row<down;row++){
+                sum+=grid[row][column];
+                if(sum>k)break;
+                int j=column;
+                j--;
+                while(sum<=k && j>=0){
+                    sum+=grid[row][j];
+                    j--;
+                }
+                if(sum>k){
+                    break;
+                }
+                else{
+                    ans++;
+                }
+
+            }
+        }
+        return ans;
+
+    }
+};
