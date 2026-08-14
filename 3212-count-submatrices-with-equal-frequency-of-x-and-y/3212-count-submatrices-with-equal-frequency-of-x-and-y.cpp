624bfdfeb0c72b3ee2d61b73de98478e0c64b597
Nilesh Sahu <nileshsahu2005@gmail.com>
2026-08-15 00:41:05 +0530

    Time: 35 ms (77.01%), Space: 120.9 MB (71.07%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,27 +1,59 @@
 class Solution {
 public:
+
+    struct Val{
+        int X=0;
+        int Y=0;
+    };
+
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        int ans = 0;
-        vector<vector<array<int, 2>>> sum(n + 1, vector<array<int, 2>>(m + 1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 'X') {
-                    sum[i + 1][j + 1][0] =
-                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] + 1;
-                    sum[i + 1][j + 1][1] = 1;
-                } else if (grid[i][j] == 'Y') {
-                    sum[i + 1][j + 1][0] =
-                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] - 1;
-                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
-                } else {
-                    sum[i + 1][j + 1][0] =
-                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0];
-                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
-                }
-                ans += (!sum[i + 1][j + 1][0] && sum[i + 1][j + 1][1]) ? 1 : 0;
+        int x=0,y=0;
+        int ans=0;
+        vector<vector<Val>>tab(grid.size(),vector<Val>(grid[0].size()));
+        if(grid[0][0]=='X'){
+            tab[0][0].X=1;
+            x++;
+        }
+        if(grid[0][0]=='Y'){
+            tab[0][0].Y=1;
+            y++;
+
+        }
+        
+        
+        
+        for(int i=1;i<grid.size();i++){
+            if(grid[i][0]=='X')x++;
+            if(grid[i][0]=='Y')y++;
+            if(x==y && x && y)ans++;
+            tab[i][0].X=x;
+            tab[i][0].Y=y; 
+
+        }
+
+        x=tab[0][0].X;
+        y=tab[0][0].Y;
+        for(int i=1;i<grid[0].size();i++){
+            if(grid[0][i]=='X')x++;
+            if(grid[0][i]=='Y')y++;
+            if(x==y && y && x)ans++;
+            tab[0][i].X=x;
+            tab[0][i].Y=y; 
+
+        }
+        for(int j=1;j<grid[0].size();j++){
+            for(int i=1;i<grid.size();i++){
+                x=0;y=0;
+                if(grid[i][j]=='X')x++;
+                if(grid[i][j]=='Y')y++;
+                tab[i][j].X=x + tab[i-1][j].X + tab[i][j-1].X - tab[i-1][j-1].X;
+                tab[i][j].Y=y + tab[i-1][j].Y + tab[i][j-1].Y - tab[i-1][j-1].Y;
+                if(tab[i][j].X == tab[i][j].Y && tab[i][j].X && tab[i][j].Y)ans++;
+
             }
         }
+
+        
         return ans;
     }
 };
